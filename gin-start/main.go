90d156077581f18a620758091491392aaf633a98
SJC <sjc@debug.online>
2019-05-13 00:45:12 +0800

    finish gin sample

--- gin-start/main.go
+++ gin-start/main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
+type data struct {
+	Name string
+	Age  int
+}
+
+var d *data
+
 func main() {
 
+	d = new(data)
+
 	router := gin.Default()
-
-	router.GET("/a.json", func(context *gin.Context) {
-
+	//获取信息
+	router.GET("/a.json", func(c *gin.Context) {
+		c.JSON(200, d)
 	})
-	router.POST("/a.json", func(context *gin.Context) {
-
+	//创建
+	router.POST("/a.json", func(c *gin.Context) {
+		//绑定payload中的JSON
+		err := c.BindJSON(d)
+		if err != nil {
+			c.String(500, "%s", fmt.Sprint(err))
+		} else {
+			c.Status(204)
+		}
 	})
-	router.PUT("/a.json", func(context *gin.Context) {
-
+	router.PUT("/a.json", func(c *gin.Context) {
+		err := c.BindJSON(d)
+		if err != nil {
+			c.String(500, "%s", fmt.Sprint(err))
+		} else {
+			c.Status(204)
+		}
 	})
-	router.DELETE("/a.json", func(context *gin.Context) {
-
+	//删除
+	router.DELETE("/a.json", func(c *gin.Context) {
+		d = new(data)
+		c.Status(204)
 	})
 	router.Run(":8887")
 }
